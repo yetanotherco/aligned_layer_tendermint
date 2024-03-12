b4168a30bedf0027636538c9095e923c3aeef6f2
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-12 19:15:46 -0300

    Init terraform project

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+# Set the variable value in *.tfvars file
+# or using the -var="hcloud_token=..." CLI option
+variable "hcloud_token" {
+  sensitive = true # Requires terraform >= 0.14
+}
+
+variable "seed_ip" {
+  deafult = "91.107.239.79"
+}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+# Create a server
+resource "hcloud_server" "alignedlayer-runner" {
+  name        = "alignedlayer-${count.index}"
+  image       = "debian-12"
+  server_type = "cx11"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+  user_data = <<EOF
+    packages:
+      - git
+      - curl
+      - jq
+      - go
+    runcmd:
+      - curl https://get.ignite.com/cli! | bash
+      - git clone https://github.com/yetanotherco/aligned_layer_tendermint.git
+      - cd aligned_layer_tendermint
+      - ignite chain build --output /usr/local/bin
+      - alignedlayerd init "alignedlayer-${count.index}" --chain-id alignedlayer
+      - curl -s ${var.seed_ip}:26657/genesis | jq '.result.genesis' > ~/.alignedlayer/config/genesis.json
+      - curl -s ${var.seed_ip}:26657/status | jq '.result.node_info.id' > .seed_id
+      - alignedlayerd config set seeds "$(cat .seed_id)@${seed_ip}:26656"
+  EOF
+}
