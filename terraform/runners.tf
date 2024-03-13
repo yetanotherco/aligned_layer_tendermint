e0bd1b0d32c62113b31ca9cc5f04d2033bb00e31
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-13 11:16:02 -0300

    Change names to random string

--- terraform/runners.tf
+++ terraform/runners.tf
@@ -6,6 +6,12 @@ variable "staking_amount" {
   default = 50000
 }
 
+resource "random_string" "random" {
+	length = 12
+	special = false
+	upper = false
+}
+
 # Create a server
 resource "hcloud_server" "alignedlayer-runner" {
   count = 1
@@ -31,25 +37,25 @@ resource "hcloud_server" "alignedlayer-runner" {
       - git clone https://github.com/yetanotherco/aligned_layer_tendermint.git
       - cd aligned_layer_tendermint
       - ignite chain build --output /usr/local/bin
-      - alignedlayerd init "alignedlayer-${count.index}" --chain-id alignedlayer
+      - alignedlayerd init "${random_string.random.result}" --chain-id alignedlayer
       - curl -s ${var.seed_ip}:26657/genesis | jq '.result.genesis' > ~/.alignedlayer/config/genesis.json
       - curl -s ${var.seed_ip}:26657/status | jq '.result.node_info.id' > .seed_id
       - alignedlayerd config set config seeds "$(cat .seed_id)@${seed_ip}:26656" --skip-validate
       - alignedlayerd config set config persistent_peers "$(cat .seed_id)@${seed_ip}:26656" --skip-validate
       - alignedlayerd config set app minimum-gas-prices "0.0025stake"
-      - alignedlayerd keys add user-${count.index}
+      - alignedlayerd keys add ${random_string.random.result}
       - # Here we need to get stake tokens
       - cat > validator.json <<EOL
         {
         	"pubkey": $(alignedlayerd tendermint show-validator),
         	"amount": "${var.staking_amount}stake",
-        	"moniker": "alignedlayer-${count.index}",
+        	"moniker": "${random_string.random.result}",
         	"commission-rate": "0.1",
         	"commission-max-rate": "0.2",
         	"commission-max-change-rate": "0.01",
         	"min-self-delegation": "1"
         }
-      - alignedlayerd tx staking create-validator validator.json --from alignedlayerd-${count.index} --node tcp://${seed_ip}:26656
+      - alignedlayerd tx staking create-validator validator.json --from ${random_string.random.result} --node tcp://${seed_ip}:26656
       - alignedlayerd start
   EOF
 }
