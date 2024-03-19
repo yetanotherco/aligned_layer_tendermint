09cd5794be03cbe1d22e5580094b12690250d55c
fborello-lambda <federicoborello@lambdaclass.com>
2024-03-19 12:02:17 -0300

    Change installation script

--- monitor/install_monitor.sh
+++ monitor/install_monitor.sh
@@ -6,11 +6,16 @@ git clone -b block_monitor https://github.com/yetanotherco/aligned_layer_tenderm
 # Move the desired folder to the current directory
 mv aligned_layer_tendermint/monitor .
 
+echo "SLACK_URL=$SLACK_URL" > monitor/.env
+
 # Clean up (optional)
 rm -rf aligned_layer_tendermint
 
+# Create python venv
 cd monitor && make setup
 
+# Setup systemd
 sudo cp monitor/block_monitor.service /etc/systemd/system/monitor.service
+sudo systemctl daemon-reload
 sudo systemctl start monitor
 sudo systemctl enable monitor
