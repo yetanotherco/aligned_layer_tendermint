a70c021e4a6d30052af7236ef8af4ad407b4646d
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-19 11:37:28 -0300

    Add spawner script
    Can be set up with crontab to execute transactions periodically.
    
    The test keyring backend is necessary to allow non-interactive execution

---
+++ spawner.sh
@@ -0,0 +1,25 @@
+# !/bin/sh
+#
+# This script send a dummy transaction from an <account> with a test keyring. It should be run from the repository root.
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <account>"
+  echo "accepts 1 arg(s), received $#"
+	exit 1
+else
+	ACCOUNT=$1
+  echo $ACCOUNT
+fi
+
+CHAIN_ID=alignedlayer
+FEES=50000stake
+
+alignedlayerd tx verification verify \
+  $(cat ./prover_examples/gnark_plonk/example/proof.base64.example) \
+  $(cat ./prover_examples/gnark_plonk/example/public_inputs.base64.example) \
+  $(cat ./prover_examples/gnark_plonk/example/verifying_key.base64.example) \
+  --keyring-backend test \
+  --from $ACCOUNT \
+  --chain-id $CHAIN_ID \
+  --fees $FEES \
+  --yes
