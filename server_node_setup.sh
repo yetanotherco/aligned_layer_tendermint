9d8a95a729a10442bcd388c6bf98ac607b08dc47
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-19 16:16:10 -0300

    fix faucet setup

--- server_node_setup.sh
+++ server_node_setup.sh
@@ -26,7 +26,7 @@ ignite chain build --release -t linux:amd64
 cd release
 tar -xzf alignedlayer_linux_amd64.tar.gz
 for server in "${servers[@]}"; do
-    scp alignedlayerd $server:/usr/local/bin
+    scp alignedlayerd $server:/home/admin
 done
 cd ..
 
@@ -54,7 +54,7 @@ for i in "${!servers[@]}"; do
 done
 
 
-ssh ${servers[0]} "rm -rf /home/admin/faucet/.faucet"
-scp -p -r "prod-sim/faucet/.faucet" "${servers[0]}:/home/admin/faucet/.faucet"
+ssh ${servers[0]} "rm -rf /home/admin/aligned_layer_tendermint/faucet/.faucet"
+scp -p -r "prod-sim/faucet/.faucet" "${servers[0]}:/home/admin/aligned_layer_tendermint/faucet/.faucet"
 
 cd ..
