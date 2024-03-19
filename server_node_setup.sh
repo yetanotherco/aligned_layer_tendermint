21c14f49ffff8a0183736302b09e38d54d2cbbe8
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-19 16:52:14 -0300

    Change binary destination to home/admin

--- server_node_setup.sh
+++ server_node_setup.sh
@@ -26,7 +26,7 @@ ignite chain build --release -t linux:amd64
 cd release
 tar -xzf alignedlayer_linux_amd64.tar.gz
 for server in "${servers[@]}"; do
-    scp alignedlayerd $server:/usr/local/bin
+    scp alignedlayerd $server:/home/admin
 done
 cd ..
 
