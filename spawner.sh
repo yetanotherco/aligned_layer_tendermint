0014bfecb5bd67ca96f1043b1778fd1b8290f05d
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-20 12:07:16 -0300

    Add proof array and manual sequence number increment

--- spawner.sh
+++ spawner.sh
@@ -1,10 +1,10 @@
-# !/bin/sh
+# !/bin/bash
 #
-# This script send two dummy transactions from an <account> with a test keyring. It should be run from the repository root.
+# This script sends dummy transactions from an <account> with a test keyring. It should be run from the repository root.
 
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <account>"
-  echo "accepts 1 arg(s), received $#"
+  echo "accepts 1 arg, received $#"
   exit 1
 else
   ACCOUNT=$1
@@ -12,24 +12,35 @@ fi
 
 CHAIN_ID=alignedlayer
 
-alignedlayerd tx verification verify \
-  $(cat ./prover_examples/gnark_plonk/example/proof.base64.example) \
-  $(cat ./prover_examples/gnark_plonk/example/public_inputs.base64.example) \
-  $(cat ./prover_examples/gnark_plonk/example/verifying_key.base64.example) \
-  --keyring-backend test \
-  --from $ACCOUNT \
-  --chain-id $CHAIN_ID \
-  --fees 20stake \
-  --yes
+# New elements can be added to the array to send more transactions
+PROOFS=(
+  "$(echo $(cat ./prover_examples/gnark_plonk/example/proof.base64.example) \
+    $(cat ./prover_examples/gnark_plonk/example/public_inputs.base64.example) \
+    $(cat ./prover_examples/gnark_plonk/example/verifying_key.base64.example))" 
+  "$(echo $(cat ./prover_examples/gnark_plonk/example/bad_proof.base64.example) \
+    $(cat ./prover_examples/gnark_plonk/example/public_inputs.base64.example) \
+    $(cat ./prover_examples/gnark_plonk/example/verifying_key.base64.example))" 
+)
 
-sleep 6
+ADDRESS=$(
+  alignedlayerd keys show julian-2-s \
+    --keyring-backend test --output json \
+    | jq .address | tr -d \"
+)
+ACCOUNT_INFO=$(alignedlayerd q auth account $ADDRESS --output json)
+NUMBER=$(echo $ACCOUNT_INFO | jq .account.value.account_number | tr -d \")
+sequence=$(echo $ACCOUNT_INFO | jq .account.value.sequence | tr -d \")
 
-alignedlayerd tx verification verify \
-  $(cat ./prover_examples/gnark_plonk/example/bad_proof.base64.example) \
-  $(cat ./prover_examples/gnark_plonk/example/public_inputs.base64.example) \
-  $(cat ./prover_examples/gnark_plonk/example/verifying_key.base64.example) \
-  --keyring-backend test \
-  --from $ACCOUNT \
-  --chain-id $CHAIN_ID \
-  --fees 20stake \
-  --yes
+for ((i = 0; i < ${#PROOFS[@]}; i++))
+do
+  proof=${PROOFS[$i]}
+  echo $proof | xargs alignedlayerd tx verification verify \
+    --keyring-backend test --from $ACCOUNT \
+    --chain-id $CHAIN_ID \
+    --fees 20stake \
+    --offline \
+    --sequence $sequence \
+    --account-number $NUMBER \
+    --yes
+  let sequence++
+done
