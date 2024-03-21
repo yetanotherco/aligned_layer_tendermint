2dd4752e294f96a3c6adf0cd2d2eda2ecf8bc9d2
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-21 16:26:28 -0300

    Add docker bash script

---
+++ docker.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [[ $# -lt 1 || (($1 == "setup" || $1 == "run") && $# -lt 2) || ($1 != "stop" && $1 != "logs" && $1 == "setup" && $1 == "run") ]]
+then
+	echo "Usage:"
+	echo -e "\t$0 <setup|run> <node_name>"
+	echo -e "\t$0 <stop|logs>"
+	exit 1
+fi
+
+CMD="$1"
+NODE_NAME="$2"
+
+if [ "$CMD" == "setup" ]
+then
+	mkdir -p prod-sim
+	mkdir prod-sim/$NODE_NAME 2>/dev/null
+	if [ $? != 0 ]
+	then
+		echo -n "A validator with name $NODE_NAME already exists. Do you want to override it? [y/N] "
+		read
+
+		if [ "$REPLY" != "y" ]
+		then
+			echo "Aborting..."
+			exit 2
+		fi
+
+		rm -r prod-sim/$NODE_NAME
+		mkdir prod-sim/$NODE_NAME
+	fi
+	
+	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up node-setup
+fi
+
+if [ "$CMD" == "run" ]
+then
+	ls prod-sim/$NODE_NAME >/dev/null 2>&1
+	if [ $? != 0 ]
+	then
+		echo "No validator config was found with that name. Try running \`$0 setup $NODE_NAME\` first"
+		exit 3
+	fi
+
+	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up -d validator-runner
+
+	ls prod-sim/$NODE_NAME/config/validator.json >/dev/null 2>&1
+	if [ $? != 0 ]  # The validator is not initialized yet
+	then
+		NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up validator-setup
+	fi
+fi
+
+if [ "$CMD" == "stop" ]
+then
+	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml stop validator-runner
+fi
+
+if [ "$CMD" == "logs" ]
+then
+	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml logs -f
+fi
