2819b7e5f6cf6c7f12b342c7901d1f18acbcec20
Manuel Bilbao <manuel.bilbao@lambdaclass.com>
2024-03-22 16:30:38 -0300

    Change paths

--- docker/docker.sh
+++ docker/docker.sh
@@ -8,13 +8,14 @@ then
 	exit 1
 fi
 
+NODES_PATH="../prod-sim"
 CMD="$1"
 NODE_NAME="$2"
 
 if [ "$CMD" == "setup" ]
 then
-	mkdir -p prod-sim
-	mkdir prod-sim/$NODE_NAME 2>/dev/null
+	mkdir -p $NODES_PATH
+	mkdir $NODES_PATH/$NODE_NAME 2>/dev/null
 	if [ $? != 0 ]
 	then
 		echo -n "A validator with name $NODE_NAME already exists. Do you want to override it? [y/N] "
@@ -26,41 +27,41 @@ then
 			exit 2
 		fi
 
-		rm -r prod-sim/$NODE_NAME
-		mkdir prod-sim/$NODE_NAME
+		rm -r $NODES_PATH/$NODE_NAME
+		mkdir $NODES_PATH/$NODE_NAME
 	fi
 
-	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up node-setup
+	NODE_NAME=$NODE_NAME docker compose -f compose/validator.docker-compose.yml up node-setup
 fi
 
 if [ "$CMD" == "run" ]
 then
-	ls prod-sim/$NODE_NAME >/dev/null 2>&1
+	ls $NODES_PATH/$NODE_NAME >/dev/null 2>&1
 	if [ $? != 0 ]
 	then
 		echo "No validator config was found with that name. Try running \`$0 setup $NODE_NAME\` first"
 		exit 3
 	fi
 
-	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up -d validator-runner
+	NODE_NAME=$NODE_NAME docker compose -f compose/validator.docker-compose.yml up -d validator-runner
 
-	ls prod-sim/$NODE_NAME/config/validator.json >/dev/null 2>&1
+	ls $NODES_PATH/$NODE_NAME/config/validator.json >/dev/null 2>&1
 	if [ $? != 0 ]  # The validator is not initialized yet
 	then
 		while $(curl -s localhost:26657/status | jq .result.sync_info.catching_up); do
 			printf '.'
 			sleep 1
 		done
-		NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml up validator-setup
+		NODE_NAME=$NODE_NAME docker compose -f compose/validator.docker-compose.yml up validator-setup
 	fi
 fi
 
 if [ "$CMD" == "stop" ]
 then
-	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml stop validator-runner
+	NODE_NAME=$NODE_NAME docker compose -f compose/validator.docker-compose.yml stop validator-runner
 fi
 
 if [ "$CMD" == "logs" ]
 then
-	NODE_NAME=$NODE_NAME docker compose -f docker/compose/validator.docker-compose.yml logs -f
+	NODE_NAME=$NODE_NAME docker compose -f compose/validator.docker-compose.yml logs -f
 fi
