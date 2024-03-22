f3ec2be7c674a0e11875fd80b8867e75ff72eab2
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-22 16:41:33 -0300

    Update send_cairo_tx.sh to use temp file

--- send_cairo_tx.sh
+++ send_cairo_tx.sh
@@ -18,15 +18,13 @@ METHOD=cosmos.tx.v1beta1.Service/BroadcastTx
 : ${NODE_RPC:="localhost:9090"}
 : ${GAS:=5000000}
 
-base64 -i $PROOF_FILE -o $PROOF_FILE.base64
-
-TRIMMED_PROOF_FILE=$(mktemp)
-cat $PROOF_FILE.base64 | tr -d '\n' > $TRIMMED_PROOF_FILE
+NEW_PROOF_FILE=$(mktemp)
+base64 -i $PROOF_FILE | tr -d '\n' > $NEW_PROOF_FILE
 
 TRANSACTION=$(mktemp)
 alignedlayerd tx verification verify-cairo "PLACEHOLDER" \
   --from $FROM --chain-id $CHAIN_ID --generate-only --gas $GAS \
-  | jq '.body.messages[0].proof=$proof' --rawfile proof $TRIMMED_PROOF_FILE \
+  | jq '.body.messages[0].proof=$proof' --rawfile proof $NEW_PROOF_FILE \
   > $TRANSACTION
 
 SIGNED=$(mktemp)
