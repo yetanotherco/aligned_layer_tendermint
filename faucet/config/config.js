c03cc0cccb82f734aa12c7e0c19f9cc4a0179d1e
tomyrd <chubyrd1@gmail.com>
2024-03-26 16:16:40 -0300

    Increase ip limit while maintaing address

--- faucet/config/config.js
+++ faucet/config/config.js
@@ -56,7 +56,7 @@ export default {
                 address: 10, 
                 // how many times each ip is allowed in a window(24h),
                 // if you use proxy, double check if the req.ip is return client's ip.
-                ip: 10
+                ip: 50
             }
         },
     ]    
