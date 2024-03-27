86028d77a42492f748beaaff5b35ea686855864c
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-27 18:48:14 -0300

    Remove warnining and formatter

--- verifiers/sp1/lib/src/lib.rs
+++ verifiers/sp1/lib/src/lib.rs
@@ -1,4 +1,3 @@
-use bincode::ErrorKind;
 use sp1_core::SP1Verifier;
 
 const ELF: &[u8] = include_bytes!("../elf/riscv32im-succinct-zkvm-elf");
@@ -10,14 +9,14 @@ pub extern "C" fn verify_sp1_proof_with_elf_ffi(
     proof_bytes: &[u8; MAX_PROOF_SIZE],
     elf_bytes: &[u8; MAX_ELF_BUFFER_SIZE],
     proof_len: usize,
-    elf_len: usize
+    elf_len: usize,
 ) -> bool {
     let real_elf = &elf_bytes[0..elf_len];
 
     if let Ok(proof) = bincode::deserialize(&proof_bytes[..proof_len]) {
         return SP1Verifier::verify(real_elf, &proof).is_ok();
     }
-    
+
     false
 }
 
@@ -27,7 +26,7 @@ pub extern "C" fn verify_sp1_proof_ffi(
     proof_len: usize,
 ) -> bool {
     if let Ok(proof) = bincode::deserialize(&proof_bytes[..proof_len]) {
-        return SP1Verifier::verify(ELF, &proof).is_ok()
+        return SP1Verifier::verify(ELF, &proof).is_ok();
     }
 
     false
@@ -54,7 +53,7 @@ mod tests {
         let mut proof_buffer = [42u8; super::MAX_PROOF_SIZE];
         let proof_size = PROOF.len();
         proof_buffer[..proof_size].clone_from_slice(PROOF);
-        let result = verify_sp1_proof_ffi(&proof_buffer, proof_size-1);
+        let result = verify_sp1_proof_ffi(&proof_buffer, proof_size - 1);
         assert!(!result)
     }
 }
