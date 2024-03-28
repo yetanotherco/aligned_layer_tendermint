4902bc5af3664f90fb9447166ca78d9e8f3843bd
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-28 12:31:43 -0300

    Use generic verify in go test

--- verifiers/sp1/sp1_test.go
+++ verifiers/sp1/sp1_test.go
@@ -9,18 +9,30 @@ import (
 
 func TestFibonacciSp1ProofVerifies(t *testing.T) {
 	fmt.Println(os.Getwd())
-	f, err := os.Open("../../prover_examples/sp1/example/fibonacci.proof")
+
+	proofFile, err := os.Open("../../prover_examples/sp1/example/fibonacci.proof")
 	if err != nil {
 		t.Errorf("could not open proof file")
 	}
 
-	proofBytes := make([]byte, sp1.MAX_PROOF_SIZE)
-	nReadBytes, err := f.Read(proofBytes)
+	proof := make([]byte, sp1.MAX_PROOF_SIZE)
+	proofSize, err := proofFile.Read(proof)
 	if err != nil {
 		t.Errorf("could not read bytes from file")
 	}
 
-	if !sp1.VerifySp1Proof(([sp1.MAX_PROOF_SIZE]byte)(proofBytes), uint(nReadBytes)) {
+	elfFile, err := os.Open("lib/elf/riscv32im-succinct-zkvm-elf")
+	if err != nil {
+		t.Errorf("could not open elf file")
+	}
+
+	elf := make([]byte, sp1.MAX_ELF_SIZE)
+	elfSize, err := elfFile.Read(elf)
+	if err != nil {
+		t.Errorf("could not read bytes from file")
+	}
+
+	if !sp1.VerifySp1ProofElf(([sp1.MAX_PROOF_SIZE]byte)(proof), ([sp1.MAX_ELF_SIZE]byte)(elf), uint(proofSize), uint(elfSize)) {
 		t.Errorf("proof did not verify")
 	}
 }
