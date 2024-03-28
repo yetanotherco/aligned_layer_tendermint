e4bdb247ff0aceda0409504bf86fc9ce81a792fc
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-28 13:04:51 -0300

    Use dynamic array for sp1 proof

--- verifiers/sp1/sp1.go
+++ verifiers/sp1/sp1.go
@@ -11,14 +11,7 @@ import (
 	"unsafe"
 )
 
-const (
-	KB             = 1024
-	MB             = KB * 1024
-	MAX_PROOF_SIZE = MB + 512*KB
-	MAX_ELF_SIZE   = 512 * KB
-)
-
-func VerifySp1ProofElf(proofBuffer [MAX_PROOF_SIZE]byte, elfBuffer [MAX_ELF_SIZE]byte, proofLen uint, elfLen uint) bool {
+func VerifySp1ProofElf(proofBuffer []byte, elfBuffer []byte, proofLen uint, elfLen uint) bool {
 	proofPtr := (*C.uchar)(unsafe.Pointer(&proofBuffer[0]))
 	elfPtr := (*C.uchar)(unsafe.Pointer(&elfBuffer[0]))
 	return (bool)(C.verify_sp1_proof_with_elf_ffi(proofPtr, elfPtr, (C.uint)(proofLen), (C.uint)(elfLen)))
