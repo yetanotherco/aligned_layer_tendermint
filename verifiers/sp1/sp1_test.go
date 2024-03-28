04d08dc6ed6a9c3f69bdce0be1ab547675066cef
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-03-28 13:31:16 -0300

    Updte sp1 go test

--- verifiers/sp1/sp1_test.go
+++ verifiers/sp1/sp1_test.go
@@ -10,29 +10,17 @@ import (
 func TestFibonacciSp1ProofVerifies(t *testing.T) {
 	fmt.Println(os.Getwd())
 
-	proofFile, err := os.Open("../../prover_examples/sp1/example/fibonacci.proof")
+	proof, err := os.ReadFile("lib/src/fibonacci.proof")
 	if err != nil {
 		t.Errorf("could not open proof file")
 	}
 
-	proof := make([]byte, sp1.MAX_PROOF_SIZE)
-	proofSize, err := proofFile.Read(proof)
-	if err != nil {
-		t.Errorf("could not read bytes from file")
-	}
-
-	elfFile, err := os.Open("lib/elf/riscv32im-succinct-zkvm-elf")
+	elf, err := os.ReadFile("lib/src/fibonacci.elf")
 	if err != nil {
 		t.Errorf("could not open elf file")
 	}
 
-	elf := make([]byte, sp1.MAX_ELF_SIZE)
-	elfSize, err := elfFile.Read(elf)
-	if err != nil {
-		t.Errorf("could not read bytes from file")
-	}
-
-	if !sp1.VerifySp1ProofElf(([sp1.MAX_PROOF_SIZE]byte)(proof), ([sp1.MAX_ELF_SIZE]byte)(elf), uint(proofSize), uint(elfSize)) {
+	if !sp1.VerifySp1ProofElf(proof, elf) {
 		t.Errorf("proof did not verify")
 	}
 }
